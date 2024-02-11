254ab1be27974e0d6794ed990c358eabc0b72a86
GustavoFernandesSP <>
2024-02-11 18:35:59 -0300

    Foi adicionado uma variável e modificada.

--- first-projet/src/main/java/org/example/Main.java
+++ first-projet/src/main/java/org/example/Main.java
@@ -4,9 +4,22 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
         System.out.println("Olá Mundo!");
+
+        // iniciando uma variável declarando que ela é do tipo int=Inteira.
+        int numero = 42;
+
+        // Imprime o valor da variável
+        System.out.println("O valor da variável é: " + numero);
+
+        // Modifica o valor da variável
+        numero = 100;
+
+        // Imprime o novo valor da variável
+        System.out.println("O novo valor da variável é: " + numero);
+
+
 
     }
 }
